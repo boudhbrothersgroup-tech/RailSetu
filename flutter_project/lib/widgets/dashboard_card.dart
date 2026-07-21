88da2d1e1ac3caa1fdae90b635ef64bff4bc7fcd
boudhbrothersgroup-tech <boudhbrothersgroup@gmail.com>
2026-07-21 20:43:37 +0530

    Update dashboard_card.dart

--- flutter_project/lib/widgets/dashboard_card.dart
+++ flutter_project/lib/widgets/dashboard_card.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+class DashboardCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const DashboardCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(16),
+        onTap: onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 40, color: Colors.blue),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
